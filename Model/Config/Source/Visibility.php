9e39ab1c2e6d69ad74ad1006713264dca810bec2
MajorKuprich <majorkuprich@gmail.com>
2020-05-31 17:26:43 +0200

    Remove views

--- Model/Config/Source/Visibility.php
+++ Model/Config/Source/Visibility.php
@@ -13,32 +13,11 @@ use Magento\Framework\Data\OptionSourceInterface;
 class Visibility implements OptionSourceInterface
 {
     const PRODUCT_PAGE = 'catalog_product_view';
-    const MINI_CART_PAGE = 'customer_section_load';
-    const CART_PAGE = 'checkout_cart_index';
-    const CHECKOUT_PAGE = 'checkout_index_index';
     const ORDER_MAIL = 'sales_email_order_items';
-    const INVOICE_NEW = 'sales_order_invoice_new';
-    const INVOICE_VIEW = 'sales_order_invoice_view';
-    const INVOICE_PRINT = 'sales_order_invoice_print';
     const ORDER_VIEW_CUSTOMER_PAGE = 'sales_order_view_' . Area::AREA_FRONTEND;
     const ORDER_VIEW_PRINT = 'sales_order_print';
     const ORDER_VIEW_ADMIN_PAGE = 'sales_order_view_' . Area::AREA_ADMINHTML;
-    const SHIPMENT_NEW = 'adminhtml_order_shipment_new';
-    const SHIPMENT_VIEW = 'adminhtml_order_shipment_view';
-    const SHIPMENT_PRINT = 'sales_shipment_print';
-    const SHIPMENT_MAIL = 'sales_email_order_shipment_items';
-    const CREDITMEMO_NEW = 'sales_order_creditmemo_new';
-    const CREDITMEMO_VIEW = 'sales_order_creditmemo_view';
-    const CREDITMEMO_PRINT = 'sales_order_creditmemo_print';
-    const INVOICE_MAIL = 'sales_email_order_invoice_items';
-    const CREDITMEMO_MAIL = 'sales_email_order_creditmemo_items';
     const ORDER_CREATE_ADMIN_PAGE = 'sales_order_create_index';
-    const INVOICE_VIEW_CUSTOMER_PAGE = 'sales_order_invoice';
-    const INVOICE_PRINT_CUSTOMER_PAGE = 'sales_order_printInvoice';
-    const SHIPMENT_VIEW_CUSTOMER_PAGE = 'sales_order_shipment';
-    const SHIPMENT_PRINT_CUSTOMER_PAGE = 'sales_order_printShipment';
-    const CREDITMEMO_VIEW_CUSTOMER_PAGE = 'sales_order_creditmemo';
-    const CREDITMEMO_PRINT_CUSTOMER_PAGE = 'sales_order_printCreditmemo';
 
     /**
      * @inheritDoc
@@ -62,25 +41,9 @@ class Visibility implements OptionSourceInterface
     {
         return [
             self::PRODUCT_PAGE => __('Product'),
-            self::MINI_CART_PAGE => __('Mini cart'),
-            self::CART_PAGE => __('Cart'),
-            self::CHECKOUT_PAGE => __('Checkout'),
             self::ORDER_MAIL => __('Order mail'),
             self::ORDER_VIEW_ADMIN_PAGE => __('Order view admin'),
             self::ORDER_VIEW_CUSTOMER_PAGE . ' ' . self::ORDER_VIEW_PRINT => __('Order view customer'),
-            self::INVOICE_MAIL => __('Invoice mail'),
-            self::INVOICE_NEW . ' ' . self::INVOICE_VIEW . ' ' . self::INVOICE_PRINT => __('Invoice view admin'),
-            self::INVOICE_VIEW_CUSTOMER_PAGE . ' ' . self::INVOICE_PRINT_CUSTOMER_PAGE => __('Invoice view customer'),
-            self::SHIPMENT_MAIL => __('Shipment mail'),
-            self::SHIPMENT_NEW . ' ' . self::SHIPMENT_VIEW . ' ' . self::SHIPMENT_PRINT =>
-                __('Shipment view admin'),
-            self::SHIPMENT_VIEW_CUSTOMER_PAGE . ' ' . self::SHIPMENT_PRINT_CUSTOMER_PAGE =>
-                __('Shipment view customer'),
-            self::CREDITMEMO_MAIL => __('Creditmemo mail'),
-            self::CREDITMEMO_NEW . ' ' . self::CREDITMEMO_VIEW . ' ' . self::CREDITMEMO_PRINT =>
-                __('Creditmemo view admin'),
-            self::CREDITMEMO_VIEW_CUSTOMER_PAGE . ' ' . self::CREDITMEMO_PRINT_CUSTOMER_PAGE =>
-                __('Creditmemo view customer'),
             self::ORDER_CREATE_ADMIN_PAGE => __('Order create admin')
         ];
     }
