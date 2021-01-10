c8568509a878377750a32678e40176241b8f2ee3
MajorKuprich <duszkajakub@gmail.com>
2021-01-10 22:13:13 +0100

    Magento 2.4.1 sort fix

---
+++ Plugin/Model/ResourceModel/Fulltext/Collection/SearchCriteriaResolver.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Greenrivers Team
+ * @copyright Copyright (c) 2021 Greenrivers
+ * @package Greenrivers_DeliveryTime
+ */
+
+namespace Greenrivers\DeliveryTime\Plugin\Model\ResourceModel\Fulltext\Collection;
+
+use Greenrivers\DeliveryTime\Plugin\Model\Config;
+use Magento\Elasticsearch\Model\ResourceModel\Fulltext\Collection\SearchCriteriaResolver as Subject;
+use Magento\Framework\Api\Search\SearchCriteria;
+
+class SearchCriteriaResolver
+{
+    /**
+     * @param Subject $subject
+     * @param SearchCriteria $result
+     * @return SearchCriteria
+     */
+    public function afterResolve(Subject $subject, SearchCriteria $result): SearchCriteria
+    {
+        $sortOrders = $result->getSortOrders();
+        unset($sortOrders[Config::DELIVERY_TIME_SORT_ORDER]);
+        $result->setSortOrders($sortOrders);
+
+        return $result;
+    }
+}
